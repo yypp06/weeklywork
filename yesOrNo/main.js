e6943b470b5cd60d5f2812060feef39386008b65
Neo <yypp06@gmail.com>
2019-03-28 17:06:40 +0800

    add jQ view hover

--- yesOrNo/main.js
+++ yesOrNo/main.js
@@ -1,5 +1,29 @@
 
 $(document).ready(function(){
+    
+    $(".yes_cover").hover(function(){
+        $(".clock_yes img").css("display", "block");
+        $(".girlcover").css("opacity", "0");
+        $(".clock_blue img").css("animation", "none");
+        }, function(){
+            $(".clock_yes img").css("display", "none");
+            $(".girlcover").css("opacity", "0.4");
+            $(".clock_blue img").css("display", "1.2s linear 0.5s infinite blink");
+        
+       
+    });
+
+    $(".no_cover").hover(function(){
+        $(".clock_no img").css("display", "block");
+        $(".mancover").css("opacity", "0");
+        $(".clock_blue img").css("display", "none");
+        }, function(){
+            $(".clock_no img").css("display", "none");
+            $(".mancover").css("opacity", "0.4");
+            $(".clock_blue img").css("display", "block");
+        
+       
+    });
   
      
 
